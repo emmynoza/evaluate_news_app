9d32bf986ea6b5b74998808384d5bdc5276848cc
Emerson Espinoza <espinozer93@gmail.com>
2021-07-28 21:55:10 -0400

    creates a regular dev build script

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -31,7 +30,6 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        }),
-        new BundleAnalyzerPlugin()
+        })
     ]
 }
