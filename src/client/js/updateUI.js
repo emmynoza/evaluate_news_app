1f47db9764ddefdd0c0ccbde539457e44bcc9099
Emerson Espinoza <espinozer93@gmail.com>
2021-08-01 13:26:06 -0400

    renders results on DOM

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -1,10 +1,25 @@
 function updateUI(data) {
+
     const results = document.getElementById('results');
+    // create list for results
+    const resultsList = document.createElement('ul').setAttribute('id', 'results-list');
+    // resets the result box
+    results.innerHTML = ''
 
-    results.innerHTML = ` <h3> Score Tag: ${data.score_tag} </h3>`
+    // creates results on DOM 
 
+    const scoreTag = `Score Tag: ${data.score_tag}`;
+    const agreement = `Agreement: ${data.agreement}`;
+    const subjectivity = `Subjectivity: ${data.subjectivity}`;
+    const confidence = `Confidence: ${data.confidence}`;
 
-    console.log(data.confidence, data.score_tag);
+    resultsList.innerHTML = `
+    <li>${scoreTag}</li>
+    <li>${agreement}</li>
+    <li>${subjectivity}</li>
+    <li>${confidence}</li>
+    `
+    results.appendChild(resultsList)
 }
 
 export { updateUI }
